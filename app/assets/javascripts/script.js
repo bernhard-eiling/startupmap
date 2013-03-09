1af6ac54fa305c874e8f7981c10280ccdb538ab5
Bernhard Eiling <bernhard.eiling@gmail.com>
2013-03-09 20:14:42 +0100

    updated infobox for cluster and marker
    now there´s only one infobox open at any time

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -24,6 +24,61 @@ $(document).ready(function() {
         //zoomOnClick: false
     })
 
+    ///////////
+    // INFOBOX
+    ///////////
+    var boxStyle = "left: 0;" +
+        "top: 0;" +
+        "z-index: -2;" +
+        "color: #FFFFFF;" +
+        "float: left;" +
+        "margin-top: 10px;" +
+        "padding: 10px;" +
+        "width: 260px;" +
+        "border: 1px solid #313436;" +
+        "text-shadow: 0px 0px 3px #000;" +
+        "-moz-border-radius: 5px;" +
+        "-webkit-border-radius: 5px;" +
+        "border-radius: 5px;" +
+        "-moz-box-shadow: 0px 1px 2px #000;" +
+        "-webkit-box-shadow: 0px 1px 2px #000;" +
+        "box-shadow: 0px 1px 2px #000;" +
+        "background-color: #313436;" +
+        "background-image: -webkit-linear-gradient(top, #313436, #222526);" +
+        "background-image: -moz-linear-gradient(top, #313436, #222526);" +
+        "background-image: -ms-linear-gradient(top, #313436, #222526);" +
+        "background-image: -o-linear-gradient(top, #313436, #222526);" +
+        "background-image: linear-gradient(top, #313436, #222526);"
+
+        function setBoxOptions(boxText) {
+            var boxOptions = {
+                content: boxText,
+                disableAutoPan: false,
+                maxWidth: 0,
+                pixelOffset: new google.maps.Size(-140, 0),
+                zIndex: null,
+                boxStyle: {
+                    //background: "url('tipbox.gif') no-repeat"
+                    opacity: 0.9,
+                    width: "280px"
+                },
+                closeBoxMargin: "2px 2px 2px 2px",
+                closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
+                infoBoxClearance: new google.maps.Size(1, 1),
+                isHidden: false,
+                pane: "floatPane",
+                enableEventPropagation: true
+            }
+            return boxOptions
+        }
+
+    var boxText = document.createElement("div");
+    boxText.style.cssText = boxStyle
+    var boxOptions = setBoxOptions(boxText)
+    var infoBox = new InfoBox(boxOptions);
+
+
+
     $('.dropdown-toggle').dropdown()
 
     // prevents dropdown checkbox clicks to close dropdown menu
@@ -37,7 +92,9 @@ $(document).ready(function() {
     fillMarkerArray()
     addMarkerToCluster()
 
+    ////////////////////
     // Search for players by name
+    ////////////////////
     $("#search-field").bind("keyup", function() {
         var name = $(this).val()
         var url = '/searchPlayer/name=' + name
@@ -49,156 +106,74 @@ $(document).ready(function() {
 
         }).done(function(data) {
             $("#search-result").html(data)
+            $(".marker-list").click(function() {
+                //console.log(findMarkerIndexByName($(this).data("name")))
+                var latLng = new google.maps.LatLng($(this).data("lat"), $(this).data("lng"))
+
+                boxText.innerHTML = $(this).data("name") + "<br><br>" + $(this).data("description")
+                infoBox.setOptions(setBoxOptions(boxText))
+
+                infoBox.open(map, markers[findMarkerIndexByName($(this).data("name"))])
+                map.panTo(latLng)
+                map.setZoom(17)
+            })
         })
     })
 
+    ///////////////////////////
     // FOR COMBINED KEY VALUE PAIRS "GRAZ AND WIEN" MAYBE PARAMS HASH?
     // Fill DOM with Marker kind
+    ///////////////////////////
     $('.replace-city').click(function() {
+        infoBox.close()
         $('.replace-city').css("background-color", "white")
         $(this).css("background-color", "blue")
         cityVal = $(this).attr("value")
         replaceMarker()
     })
     $('.replace-kind').click(function() {
+        infoBox.close()
         $('.replace-kind').css("background-color", "white")
         $(this).css("background-color", "blue")
         kindVal = $(this).attr("value")
         replaceMarker()
     })
 
-        function replaceMarker() {
-            var url = '/placeMarker/kind=' + kindVal + "/city=" + cityVal
-            console.log(kindVal + ", " + cityVal)
-
-            $.ajax({
-                url: url,
-                dataType: "text"
-
-            }).done(function(data) {
-                $("#marker-container").html(data)
-                fillMarkerArray()
-                addMarkerToCluster()
-            })
+    function findMarkerIndexByName(name) {
+        var returnIndex = -1
+        for (var i = 0; i < markers.length; i++) {
+            //console.log("markername: " + markers[i].name + ", paraname: " + name)
+            if (markers[i].name == name) {
+                returnIndex = i
+            }
         }
+        return returnIndex
+    }
 
-        function fillMarkerArray() {;
+    function replaceMarker() {
+        var url = '/placeMarker/kind=' + kindVal + "/city=" + cityVal
+        console.log(kindVal + ", " + cityVal)
 
-            markers.length = 0
+        $.ajax({
+            url: url,
+            dataType: "text"
 
-            $(".marker").each(function() {
-                // marker of lowest layer
-                var markerImage = new google.maps.MarkerImage(
-                    "assets/markermaxhell.png",
-                new google.maps.Size(31, 36))
-                var latLng = new google.maps.LatLng($(this).data("lat"), $(this).data("lng"))
-                var marker = new google.maps.Marker({
-                    position: latLng,
-                    icon: markerImage,
-                    name: $(this).data("name"),
-                    description: $(this).data("description")
-                })
+        }).done(function(data) {
+            $("#marker-container").html(data)
+            fillMarkerArray()
+            addMarkerToCluster()
+        })
+    }
 
-                markers.push(marker)
-            })
-        }
+    function fillMarkerArray() {;
+        // marker of lowest layer
+        var markerImage = new google.maps.MarkerImage("assets/markermaxhell.png", new google.maps.Size(31, 36))
 
-        function addInfoBox() {
+        markers.length = 0
 
-        }
-
-        function addMarkerToCluster() {
-            var styles = [
-                [{
-                    url: 'assets/markerhell.png',
-                    height: 48,
-                    width: 41,
-                    anchor: [20, 0],
-                    textColor: '#000000',
-                    textSize: 15,
-                    fontWeight: "normal"
-                }, {
-                    url: 'assets/marker.png',
-                    height: 60,
-                    width: 51,
-                    anchor: [25, 0],
-                    textColor: '#000000',
-                    textSize: 15,
-                    fontWeight: "normal"
-                }, {
-                    url: 'assets/marker.png',
-                    height: 60,
-                    width: 51,
-                    anchor: [25, 0],
-                    textColor: '#000000',
-                    textSize: 15,
-                    fontWeight: "normal"
-                }]
-            ]
-
-            markerCluster.setStyles(styles[0])
-            markerCluster.setZoomOnClick(false)
-
-            markerCluster.clearMarkers()
-            markerCluster.addMarkers(markers)
-
-        }
-
-        function initMarker() {
-            //////////////////////
-            // MARKER
-            //////////////////////
-
-            var boxStyle = "left: 0;" +
-                "top: 0;" +
-                "z-index: -2;" +
-                "color: #FFFFFF;" +
-                "float: left;" +
-                "margin-top: 10px;" +
-                "padding: 10px;" +
-                "width: 260px;" +
-                "border: 1px solid #313436;" +
-                "text-shadow: 0px 0px 3px #000;" +
-                "-moz-border-radius: 5px;" +
-                "-webkit-border-radius: 5px;" +
-                "border-radius: 5px;" +
-                "-moz-box-shadow: 0px 1px 2px #000;" +
-                "-webkit-box-shadow: 0px 1px 2px #000;" +
-                "box-shadow: 0px 1px 2px #000;" +
-                "background-color: #313436;" +
-                "background-image: -webkit-linear-gradient(top, #313436, #222526);" +
-                "background-image: -moz-linear-gradient(top, #313436, #222526);" +
-                "background-image: -ms-linear-gradient(top, #313436, #222526);" +
-                "background-image: -o-linear-gradient(top, #313436, #222526);" +
-                "background-image: linear-gradient(top, #313436, #222526);"
-
-                function setBoxOptions(boxText) {
-                    var boxOptions = {
-                        content: boxText,
-                        disableAutoPan: false,
-                        maxWidth: 0,
-                        pixelOffset: new google.maps.Size(-140, 0),
-                        zIndex: null,
-                        boxStyle: {
-                            //background: "url('tipbox.gif') no-repeat"
-                            opacity: 0.9,
-                            width: "280px"
-                        },
-                        closeBoxMargin: "2px 2px 2px 2px",
-                        closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
-                        infoBoxClearance: new google.maps.Size(1, 1),
-                        isHidden: false,
-                        pane: "floatPane",
-                        enableEventPropagation: true
-                    }
-                    return boxOptions
-                }
-
-
-
-                /*
         $(".marker").each(function() {
-            //var infowindowSingle = new google.maps.InfoWindow()
+
+
             var latLng = new google.maps.LatLng($(this).data("lat"), $(this).data("lng"))
             var marker = new google.maps.Marker({
                 position: latLng,
@@ -207,97 +182,80 @@ $(document).ready(function() {
                 description: $(this).data("description")
             })
 
-            var boxText = document.createElement("div");
-
-            boxText.style.cssText = boxStyle
-
-            var boxOptions = setBoxOptions(boxText)
-
-            boxText.innerHTML = $(this).data("name") + "<br><br>" + $(this).data("description")
-
-            var ib = new InfoBox(boxOptions)
-
-            makeInfoBoxEvent(map, ib, marker)
-
+            google.maps.event.addListener(marker, 'click', function() {
+                boxText.innerHTML = marker.name + "<br><br>" + marker.description
+                infoBox.setOptions(setBoxOptions(boxText))
+                infoBox.open(map, marker)
+            })
             markers.push(marker)
-
         })
-*/
+    }
 
+    function addMarkerToCluster() {
+        var styles = [
+            [{
+                url: 'assets/markerhell.png',
+                height: 48,
+                width: 41,
+                anchor: [20, 0],
+                textColor: '#000000',
+                textSize: 15,
+                fontWeight: "normal"
+            }, {
+                url: 'assets/marker.png',
+                height: 60,
+                width: 51,
+                anchor: [25, 0],
+                textColor: '#000000',
+                textSize: 15,
+                fontWeight: "normal"
+            }, {
+                url: 'assets/marker.png',
+                height: 60,
+                width: 51,
+                anchor: [25, 0],
+                textColor: '#000000',
+                textSize: 15,
+                fontWeight: "normal"
+            }]
+        ]
 
-                markerCluster.clearMarkers()
+        markerCluster.setStyles(styles[0])
+        markerCluster.setZoomOnClick(false)
 
-                console.log(markerCluster)
+        markerCluster.clearMarkers()
+        markerCluster.addMarkers(markers)
 
-                markerCluster.addMarkers(markers)
+        google.maps.event.addListener(markerCluster, 'clusterclick', function(ev) {
 
-                function makeInfoBoxEvent(map, ib, marker) {
-                    google.maps.event.addListener(marker, 'click', function() {
-                        ib.open(map, marker)
-                    })
-                }
+            var boxHTML = ""
 
+            for (var i = 0; i < ev.getSize(); i++) {
+                //console.log(ev.getMarkers()[i])
+                boxHTML += "<a href='#' class='cluster-marker' data-id=" + i + " + data-lat=" + ev.getMarkers()[i].position.ib + " data-lng=" + ev.getMarkers()[i].position.jb + ">" + ev.getMarkers()[i].name + "</a><br>"
 
-                //////////////////////
-                // MARKERCLUSTERER
-                //////////////////////
-                // styles of different zoom levels
+            }
+            // console.log(boxHTML)
+            // console.log(boxText)
+            boxText.innerHTML = boxHTML
+            infoBox.setOptions(setBoxOptions(boxText))
+            infoBox.open(map, ev.getMarkers()[0])
 
-                //markerCluster.clearMarkers()
-                //markerCluster.addMarkers(markers)
+            var markersInCluster = ev.getMarkers()
 
-                //console.log(markerCluster.getMarkers())
+            google.maps.event.addListener(infoBox, 'domready', function(ev) {
 
+                $(".cluster-marker").click(function() {
+                    var clusterItemPos = new google.maps.LatLng($(this).data("lat"), $(this).data("lng"))
 
-            var boxTextCluster = document.createElement("div");
+                    boxText.innerHTML = markersInCluster[parseInt($(this).data("id"))].name + "<br><br>" + markersInCluster[parseInt($(this).data("id"))].description
+                    infoBox.setOptions(setBoxOptions(boxText))
+                    infoBox.open(map, markersInCluster[parseInt($(this).data("id"))])
 
-            boxTextCluster.style.cssText = boxStyle
-
-            var boxOptionsCluster = setBoxOptions(boxTextCluster)
-
-            var infoBoxCluster = new InfoBox(boxOptionsCluster);
-
-            google.maps.event.addListener(markerCluster, 'clusterclick', function(ev) {
-                //console.log(ev.getMarkers()[i].name)
-                var boxHTML = ""
-                var latLngCluster = ev.getCenter()
-                var markerCluster = new google.maps.Marker({
-                    position: latLngCluster,
-                    icon: markerImage
-                })
-                for (var i = 0; i < ev.getSize(); i++) {
-                    console.log(ev.getMarkers()[i])
-                    boxHTML += "<a href='#' class='cluster-marker' data-id=" + i + " + data-lat=" + ev.getMarkers()[i].position.ib + " data-lng=" + ev.getMarkers()[i].position.jb + ">" + ev.getMarkers()[i].name + "</a><br>"
-
-                }
-                boxTextCluster.innerHTML = boxHTML
-                infoBoxCluster.open(map, markerCluster)
-
-                var markersInfoBox = ev.getMarkers()
-
-                google.maps.event.addListener(infoBoxCluster, 'domready', function(ev) {
-                    //console.log(infoBoxCluster)
-                    $(".cluster-marker").click(function() {
-                        var clusterItemPos = new google.maps.LatLng($(this).data("lat"), $(this).data("lng"))
-                        infoBoxCluster.close(map, markerCluster)
-                        //console.log(parseInt($(this).data("id")))
-
-                        var boxText2 = document.createElement("div");
-
-                        boxText2.style.cssText = boxStyle
-
-                        var boxOptions2 = setBoxOptions(boxText2)
-
-                        boxText2.innerHTML = markersInfoBox[parseInt($(this).data("id"))].name + "<br><br>" + markersInfoBox[parseInt($(this).data("id"))].description
-
-                        ib2 = new InfoBox(boxOptions2)
-
-                        //makeInfoBoxEvent(map, ib2, markersInfoBox[parseInt($(this).data("id"))])
-                        ib2.open(map, markersInfoBox[parseInt($(this).data("id"))])
-                        map.panTo(clusterItemPos)
-                        map.setZoom(17)
-                    })
+                    map.panTo(clusterItemPos)
+                    map.setZoom(17)
                 })
             })
-        }
+        })
+    }
 })
